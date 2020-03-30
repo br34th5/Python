f8dc741b386ec03fec0c20e39fb8acffd81f1675
br34th5 <ek4music@gmail.com>
2020-03-30 20:47:57 +0300

    dabar automatiskai kuria folder jeigu nera pagal search term

--- Web Scraper/images.py
+++ Web Scraper/images.py
@@ -2,11 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 from PIL import Image
 from io import BytesIO
+import os
 
 
 def StartSearch():
     search = input("Search for:")
     params = {"q": search}
+    dir_name = search.replace(" ", "_").lower()
+    
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
+    
     r = requests.get("https://www.bing.com/images/search", params = params)
 
     soup = BeautifulSoup(r.text, "html.parser")
@@ -18,7 +24,7 @@ def StartSearch():
         title = item.attrs["href"].split("/")[-1]
         try:
             img = Image.open(BytesIO(img_obj.content))
-            img.save("./scraped_images" + title, img.format)
+            img.save("./" + dir_name + "/" + title, img.format)
         except:
             print("Could not save the file")
     StartSearch()
